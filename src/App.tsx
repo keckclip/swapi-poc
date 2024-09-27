9948f5abeb3bb84ede5bfa5f2c6ddc6f30061b7d
keckclip <keckclip@googlemail.com>
2024-09-27 14:28:11 -0700

    [refactor] Extracted Menu to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
 import "./App.css";
+import { Menu } from "./components/Menu";
 
-import MainRouter  from "./router/MainRouter";
+import MainRouter from "./router/MainRouter";
 
 function App() {
   return (
     <>
       <header>SWAPI React App</header>
       <nav>
-        Menu
-        <ul>
-          <li>
-            <a href="/films">Films</a>
-          </li>
-          <li>
-            <a href="/people">People</a>
-          </li>
-          <li>
-            <a href="/planets">Planets</a>
-          </li>
-          <li>
-            <a href="/species">Species</a>
-          </li>
-          <li>
-            <a href="/starships">Starships</a>
-          </li>
-          <li>
-            <a href="/vehicles">Vehicles</a>
-          </li>
-        </ul>
+        <Menu />
       </nav>
       <main>
         <MainRouter />
@@ -38,3 +19,5 @@ function App() {
 }
 
 export default App;
+
+
---
+++ src/components/Menu.tsx
@@ -0,0 +1,27 @@
+export const Menu = () => {
+  return (
+    <div>
+      Menu
+      <ul>
+        <li>
+          <a href="/films">Films</a>
+        </li>
+        <li>
+          <a href="/people">People</a>
+        </li>
+        <li>
+          <a href="/planets">Planets</a>
+        </li>
+        <li>
+          <a href="/species">Species</a>
+        </li>
+        <li>
+          <a href="/starships">Starships</a>
+        </li>
+        <li>
+          <a href="/vehicles">Vehicles</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
