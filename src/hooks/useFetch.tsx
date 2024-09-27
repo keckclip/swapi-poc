d969f9db93bfb246d51234181f0603f5cfd926b9
keckclip <keckclip@googlemail.com>
2024-09-27 15:28:08 -0700

    [feature] Added basic hook for data fetching

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (url: string) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+  return { data, loading };
+};
