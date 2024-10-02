028bd1efbba277a7c8cbb50cd7b16bf98954cbff
keckclip <keckclip@googlemail.com>
2024-10-01 21:01:33 -0700

    [polish] Made minor formatting changes

--- src/components/ListItems.tsx
+++ src/components/ListItems.tsx
@@ -1,10 +1,15 @@
 const baseUrl = "https://swapi.dev/api/";
 
 export const ListItems = ({ items }) => {
-  return (
-    items.map(item => {
-      const href = item.url.replace(baseUrl, '')
-      return (<li key={item.url}><a href={href}>{item.name}{item.title}</a></li>)
-    })
-  );
+  return items.map((item) => {
+    const href = item.url.replace(baseUrl, "");
+    return (
+      <li key={item.url}>
+        <a href={href}>
+          {item.name}
+          {item.title}
+        </a>
+      </li>
+    );
+  });
 };
--- src/components/ShowView.tsx
+++ src/components/ShowView.tsx
@@ -1,8 +1,9 @@
 import { DetailView } from "../components/DetailView";
 import { ListView } from "../components/ListView";
 
-export const ShowView = ({ id, baseUrl, pageTitle }) => id ? (
-  <DetailView url={`${baseUrl}${id}`} pageTitle={pageTitle} />
-) : (
-  <ListView baseUrl={baseUrl} pageTitle={pageTitle} />
-);
+export const ShowView = ({ id, baseUrl, pageTitle }) =>
+  id ? (
+    <DetailView url={`${baseUrl}${id}`} pageTitle={pageTitle} />
+  ) : (
+    <ListView baseUrl={baseUrl} pageTitle={pageTitle} />
+  );
